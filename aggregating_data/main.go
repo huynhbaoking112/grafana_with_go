60e9217aed494b3949662b1da8200e69efb4367f
BaoKing <huynhbaoking@gmail.com>
2025-02-22 17:48:30 +0700

    solution 3

--- aggregating_data/main.go
+++ aggregating_data/main.go
@@ -15,6 +15,7 @@ import (
 // 	fmt.Println(match)
 // 	fmt.Println(time.Since(start))
 // } => { 11 ANNA 3.0019714s }
+//==========================================================================
 
 // Áp goroutine 1
 
@@ -47,51 +48,90 @@ import (
 // ANNA
 // 2.0014738s}
 
+//==========================================================================
+// CÁCH 3
+// func main() {
+// 	start := time.Now()
+// 	userName := fetchUser()
+
+// 	likesChan := make(chan int)
+// 	matchChan := make(chan string)
+
+// 	// Dùng sync.WaitGroup để đợi goroutines hoàn thành
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+
+// 	// Goroutine lấy likes
+// 	go func() {
+// 		defer wg.Done()
+// 		likesChan <- fetchUserLikes(userName)
+// 	}()
+
+// 	// Goroutine lấy match
+// 	go func() {
+// 		defer wg.Done()
+// 		matchChan <- fetchUserMatch(userName)
+// 	}()
+
+// 	// Goroutine đóng channel sau khi tất cả hoàn thành
+// 	go func() {
+// 		wg.Wait()
+// 		close(likesChan)
+// 		close(matchChan)
+// 	}()
+
+// 	// Nhận dữ liệu không cần WaitGroup.Wait()
+// 	var likes int
+// 	var match string
+// 	for i := 0; i < 2; i++ {
+// 		select {
+// 		case likes = <-likesChan:
+// 		case match = <-matchChan:
+// 		}
+// 	}
+
+//		fmt.Println(likes)
+//		fmt.Println(match)
+//		fmt.Println(time.Since(start))
+//	}
+//
+// ==========================================================================
 func main() {
+
 	start := time.Now()
 	userName := fetchUser()
 
-	likesChan := make(chan int)
-	matchChan := make(chan string)
-
-	// Dùng sync.WaitGroup để đợi goroutines hoàn thành
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	// Goroutine lấy likes
-	go func() {
-		defer wg.Done()
-		likesChan <- fetchUserLikes(userName)
-	}()
-
-	// Goroutine lấy match
-	go func() {
-		defer wg.Done()
-		matchChan <- fetchUserMatch(userName)
-	}()
-
-	// Goroutine đóng channel sau khi tất cả hoàn thành
-	go func() {
-		wg.Wait()
-		close(likesChan)
-		close(matchChan)
-	}()
-
-	// Nhận dữ liệu không cần WaitGroup.Wait()
-	var likes int
-	var match string
-	for i := 0; i < 2; i++ {
-		select {
-		case likes = <-likesChan:
-		case match = <-matchChan:
-		}
+	type person struct {
+		name string
+		age  int
 	}
 
-	fmt.Println(likes)
-	fmt.Println(match)
+	var king person
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func(p *person, n string) {
+		defer wg.Done()
+		p.age = fetchUserLikes(n)
+	}(&king, userName)
+
+	go func(p *person, n string) {
+		defer wg.Done()
+		p.name = fetchUserMatch(n)
+	}(&king, userName)
+
+	wg.Wait()
+
+	fmt.Println(king.age)
+	fmt.Println(king.name)
 	fmt.Println(time.Since(start))
 }
 
+// PS C:\Users\huynh\OneDrive\Desktop\learngo\grafana> go run .\aggregating_data\
+// 11
+// ANNA
+// 2.0035761s
 func fetchUser() string {
 
 	time.Sleep(time.Second * 1)
